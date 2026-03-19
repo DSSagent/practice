72a15f37db54f2c3037d0e87696fdb91204bd01f
Sebastian Mejia <Sebastianmejia200625@gmail.com>
2026-03-19 07:20:47 -0400

    add two_sum_different function to find indices of two numbers that add up to target

--- two_sum.py
+++ two_sum.py
@@ -3,6 +3,13 @@ def two_sum(nums, target):
         if i + 1 < len(nums):
             if nums[i] + nums[i + 1] == target:
                 print([i, i + 1])
+
+
+def two_sum_different(nums, target):
+    for i in nums:
+        if i < target:
+            if target - i in nums:
+                print([nums.index(i), nums.index(target - i)])
                 
 
 
@@ -10,5 +17,6 @@ def main():
     nums = [2, 7, 11, 15]
     target = 9
     two_sum(nums, target)
-    
+    two_sum_different(nums, target)
+
 main()
