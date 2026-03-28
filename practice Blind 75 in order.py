b6dd45b8a9d6419253d8a14128ee4336c2d1507d
Sebastian Mejia <Sebastianmejia200625@gmail.com>
2026-03-28 11:52:42 -0400

    search_in_rotated_sorted_array

--- practice Blind 75 in order.py
+++ practice Blind 75 in order.py
@@ -91,21 +91,45 @@ def min_sorted_rotated_array(nums):
     left, right = 0, len(nums) - 1
     while left < right:
         mid = (left + right) // 2
+        print(f"Left: {left}, Mid: {mid}, Right: {right}")
         if nums[mid] > nums[right]:
             left = mid + 1
         else:
             right = mid
     return nums[left]
 
+def search_in_rotated_sorted_array(nums, target):
+    if len(nums) == 0 or len(nums) > 5000:
+        return None
+    if nums[0] <= -10000 or nums[len(nums) - 1] >= 10000:
+        return None
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right-left) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1          
+    return -1
 
 def main():
     nums = [1, 2, 3, 4]
     nums2 = [5,6,7]
-    nums3 = [-2, 0, -1]
+    nums3 = [4,5,6,1,2,3]
     nums4 = [-3,2,5,-1]
     nums5 = [-10,11]
-    print(min_sorted_rotated_array(nums))
-    print(min_sorted_rotated_array(nums2))
+    print(search_in_rotated_sorted_array(nums3, 1))
+    print(search_in_rotated_sorted_array(nums3, 6))
+    print(search_in_rotated_sorted_array(nums3, 9))
+    #print(min_sorted_rotated_array(nums3))
 
 
 main()
